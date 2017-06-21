24ba4ce15d3751bf4184d81cf08a45f9d15d16f3
gongcy <gongcy126@126.com>
2017-06-21 19:40:55 +0800

    modify web.xml

--- src/java/dao/ManagerDao.java
+++ src/java/dao/ManagerDao.java
@@ -5,10 +5,36 @@
  */
 package dao;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import pojo.Manager;
+
 /**
  *
  * @author Administrator
  */
 public class ManagerDao {
     
+    @Autowired
+    private SessionFactory sessionFactory;
+    
+    public void insert(Manager manager) {
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+        session.save(manager);
+        tx.commit();
+        session.disconnect();
+//        session.close();
+    }
+    
+    public Manager getStudent(String magno) {
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.getTransaction();
+        Manager m = (Manager)session.get(Manager.class, magno);
+        tx.commit();
+        session.disconnect();
+        return m;
+    }
 }
