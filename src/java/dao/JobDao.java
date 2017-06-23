86434e1c05201ef33b1957b8aee1963f8cb14227
gongcy <gongcy126@126.com>
2017-06-23 12:04:28 +0800

    modify dao

--- src/java/dao/JobDao.java
+++ src/java/dao/JobDao.java
@@ -5,10 +5,89 @@
  */
 package dao;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import pojo.Job;
+
 /**
  *
  * @author Administrator
  */
 public class JobDao {
     
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public void insert(Job manager) {
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            session.beginTransaction();
+            session.save(manager);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+    }
+
+    public void delete(String magno) {
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            session.beginTransaction();
+            Job m = (Job) session.load(Job.class, magno);
+            session.delete(m);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+    }
+
+    public void update(String magno) {
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            session.beginTransaction();
+            Job m = (Job) session.get(Job.class, magno);
+            session.update(m);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+    }
+
+    public Job getManager(String magno) {
+        Session session = null;
+        Job m = null;
+        try {
+            session = sessionFactory.openSession();
+            session.beginTransaction();
+            m = (Job) session.get(Job.class, magno);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+            return m;
+        }
+    }
 }
